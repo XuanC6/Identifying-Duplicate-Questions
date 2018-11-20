bc0c1571368a70a97c41e9c169385076d16842b8
XuanC6 <33044517+XuanC6@users.noreply.github.com>
2018-11-20 16:06:04 -0500

    update

---
+++ model/src/main.py
@@ -0,0 +1,158 @@
+import time
+import numpy as np
+import sys, os, glob
+import tensorflow as tf
+
+from reader import Reader
+from trainer import Trainer
+from config import Configuration
+
+
+class Classifier:
+
+    def __init__(self):
+        self.config = Configuration()
+        self.reader = Reader(self.config)
+        self.trainer = Trainer()
+        self.load_data()
+
+
+    def load_data(self):
+        data = self.reader.load_data()
+        self.train_set = data['train']
+        self.val_set = data['valid']
+
+
+    def train(self, restore):
+        if restore == False:
+            train_files = glob.glob(self.config.log_train_dir+'/*')
+            for train_file in train_files:
+                os.remove(train_file)
+
+        with tf.Graph().as_default(), tf.Session() as sess:
+            model = self.config.model(self.config)
+            init = tf.global_variables_initializer()
+            saver = tf.train.Saver()
+            sess.run(init)
+            
+            # assign word_embedding
+            if self.config.word_embedding_pretrained:
+                word_embedding = np.loadtxt(self.config.wordvecs_path, dtype=np.float32)
+                with tf.variable_scope("Embed", reuse=True):
+                    embedding1 = tf.get_variable("embedding1", 
+                                                 [self.config.num_words, self.config.wordvec_size])
+                    embedding2 = tf.get_variable("embedding2", 
+                                                 [self.config.num_words, self.config.wordvec_size])
+                    e1_assign = embedding1.assign(word_embedding)
+                    e2_assign = embedding2.assign(word_embedding)
+            
+            sess.run([e1_assign, e2_assign])
+
+            best_valid_acc = 0.0
+            best_valid_epoch = 0
+
+            if restore:
+                print("Continue Training")
+                saver.restore(sess, self.config.model_path)
+            
+            writer = tf.summary.FileWriter(self.config.log_graph_dir, sess.graph)
+
+            with open(self.config.log_train_acc_path, "w") as train_acc_fp,\
+                open(self.config.log_valid_acc_path, "w") as valid_acc_fp:
+
+                for epoch in range(self.config.num_epoch):
+                    start_time = time.time()
+
+                    # whether decay lr
+                    if self.config.lr_decay and epoch+1 > self.config.lr_decay_epoch:
+                        lr_decay = self.config.lr_decay_rate**max(epoch+1-self.config.lr_decay_epoch, 0.0)
+                        sess.run(tf.assign(model.learning_rate, self.config.learning_rate*lr_decay))
+
+                    # train one epoch
+                    print('='*40)
+                    print(("Epoch %d, Learning Rate: %.4f")%(epoch+1, sess.run(model.learning_rate)))
+                    print(("best valid acc now: %.4f") % best_valid_acc)
+                    print(("best valid epoch now: %d") % best_valid_epoch)
+                    loss = self.trainer.train_one_epoch(self.train_set, model, sess, self.config.shuffle_data)
+                    print(('\ntrain loss: %.4f') % loss)
+
+                    # evaluate on train data every 5 epoches
+                    if (epoch+1)%5 == 0:
+                        train_metrics = self.trainer.evaluate(self.train_set, model, sess)
+                        print(('train acc: %.4f')%train_metrics["acc"])
+                        train_acc_fp.write("%d: %.4f\n"%(epoch+1, train_metrics["acc"]))
+
+                    # evaluate on valid data
+                    metrics = self.trainer.evaluate(self.val_set, model, sess)
+                    print(('valid acc: %.4f')%metrics["acc"])
+                    valid_acc_fp.write("%d: %.4f\n"%(epoch+1, metrics["acc"]))
+
+                    # save model if save_by_best_valid
+                    if metrics["acc"] > best_valid_acc:
+                        best_valid_acc = metrics["acc"]
+                        best_valid_epoch = epoch+1
+                        if self.config.model_save_by_best_valid:
+                            saver.save(sess, self.config.model_path)
+
+                    # save model if not save_by_best_valid
+                    if not self.config.model_save_by_best_valid and (epoch+1)%self.config.model_save_period==0:
+                        saver.save(sess, self.config.model_path)
+
+                    # break if save_by_best_valid
+                    if self.config.model_save_by_best_valid and epoch+1-best_valid_epoch > self.config.early_stop_epoch:
+                        break
+
+                    print("time per epoch is %.2f min"%((time.time()-start_time)/60.0))
+
+            # save final model if not save_by_best_valid
+            if not self.config.model_save_by_best_valid:
+                saver.save(sess, self.config.model_path)
+
+            print(("\nbest valid acc: %.4f")%best_valid_acc)
+            metrics = self.trainer.evaluate(self.val_set, model, sess)
+            print(('*'*10 + 'test acc: %.4f')%metrics["acc"])
+            
+            writer.close()
+
+
+    def test(self):
+        test_set = self.val_set
+#        test_set = self.train_set
+        with tf.Graph().as_default(), tf.Session() as sess:
+            model = self.config.model(self.config)
+            init = tf.global_variables_initializer()
+            saver = tf.train.Saver()
+            sess.run(init)
+            saver.restore(sess, self.config.model_path)
+
+            print("testing..............")
+            metrics = self.trainer.evaluate(test_set, model, sess)
+            print(("accuracy: %.4f")%metrics["acc"])
+            print(("precision: %.4f")%metrics["precision"])
+            print(("recall: %.4f")%metrics["recall"])
+            print(("F1 score: %.4f")%metrics["F1 score"])
+
+            print(("TP: %d")%metrics["TP"])
+            print(("TN: %d")%metrics["TN"])
+            print(("FP: %d")%metrics["FP"])
+            print(("FN: %d")%metrics["FN"])
+
+
+if __name__ == "__main__":
+
+    classifier = Classifier()
+
+    if len(sys.argv) > 1:
+        if sys.argv[1]=="continue":
+            restore=True
+            classifier.train(restore)
+        elif sys.argv[1]=="train":
+            restore = False
+            classifier.train(restore)
+        elif sys.argv[1]=="test":
+            classifier.test()
+    else:
+        restore=False
+        classifier.train(restore)
+        classifier.test()
+
---
+++ model/src/models.py
@@ -0,0 +1,137 @@
+import os
+import sys
+import tensorflow as tf
+
+base_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+utils_dir = os.path.join(base_dir, "utils")
+sys.path.append(utils_dir)
+
+
+class BiRNNModel:
+
+    def __init__(self, config):
+        self.config = config
+
+        self.num_steps = config.num_steps
+        self.num_words = config.num_words
+        self.wordvec_size = config.wordvec_size
+        self.batch_size = config.batch_size
+        self.initializer = config.initializer
+        self.rnn_initializer = config.rnn_initializer
+
+        self.add_placeholders()
+        inputs = self.add_embedding()
+        self.compute_loss(inputs)
+        self.add_train_op()
+
+
+    def add_placeholders(self):
+        dim1 = self.batch_size
+        dim2 = self.num_steps
+        
+        self.input1 = tf.placeholder(tf.int32, [dim1, dim2], name="ques1")
+        self.input2 = tf.placeholder(tf.int32, [dim1, dim2], name="ques2")
+        self.length1 = tf.placeholder(tf.int32, [dim1], name="length1")
+        self.length2 = tf.placeholder(tf.int32, [dim1], name="length2")
+        self.labels = tf.placeholder(tf.int32, [dim1], name="labels")
+        self.dropout = tf.placeholder(tf.float32, [], name="dropout")
+        self.training = tf.placeholder_with_default(False, [], name="training")
+
+
+    def add_embedding(self):
+        def add_embedding_helper(name):
+            embedding = tf.get_variable(
+                name,
+                [self.num_words, self.wordvec_size],
+                initializer = tf.random_uniform_initializer(-0.5, 0.5),
+                trainable = self.config.word_embedding_trainable)
+            return embedding
+        
+        with tf.variable_scope("Embed"):
+            embedding1 = add_embedding_helper("embedding1")
+            embedding2 = add_embedding_helper("embedding2")
+        
+        vec_input1 = tf.nn.embedding_lookup(embedding1, self.input1)
+        vec_input2 = tf.nn.embedding_lookup(embedding2, self.input2)
+
+        return [vec_input1, vec_input2]
+
+
+    def _run_rnn(self, inputs):
+        # calculate outputs
+        def create_rnncells():
+            rnncells = [None for _ in range(4)]
+            for i in range(4):
+#                 _rnncell = tf.nn.rnn_cell.GRUCell(num_units=self.config.num_units,
+#                                                   kernel_initializer=self.rnn_initializer())
+#                 rnncells[i] = tf.nn.rnn_cell.DropoutWrapper(_rnncell,
+#                                                             output_keep_prob=self.dropout)
+                 rnncells[i] = tf.nn.rnn_cell.GRUCell(num_units=self.config.num_units,
+                                                   kernel_initializer=self.rnn_initializer())
+            return rnncells
+
+        rnncells = create_rnncells()
+        with tf.variable_scope("Bidirectional_RNN1"):
+            outputs1, state1 = tf.nn.bidirectional_dynamic_rnn(rnncells[0], rnncells[1], inputs[0],
+                                                               sequence_length = self.length1,
+                                                               dtype=tf.float32)
+        with tf.variable_scope("Bidirectional_RNN2"):
+            outputs2, state2 = tf.nn.bidirectional_dynamic_rnn(rnncells[2], rnncells[3], inputs[1],
+                                                               sequence_length = self.length2,
+                                                               dtype=tf.float32)
+
+        fw1, bw1 = state1
+        fw2, bw2 = state2
+        rnn_output = tf.concat(values = [fw1, bw1, fw2, bw2], axis = -1)
+        
+        return rnn_output
+
+
+    def compute_loss(self, inputs):
+        hidden_layer = self._run_rnn(inputs)
+        
+        for n_node in self.config.mlp_hidden_nodes:
+            hidden_layer = tf.layers.dense(hidden_layer, n_node, 
+                                           activation = tf.nn.elu,
+                                           kernel_initializer = self.initializer())
+            hidden_layer = tf.layers.dropout(hidden_layer, self.config.dropout,
+                                             training = self.training)
+
+        logits = tf.layers.dense(hidden_layer, 1,
+                                 kernel_initializer = self.initializer(),
+                                 name = 'logits')
+
+        scores = tf.nn.sigmoid(logits, name="predict_probs")
+        loss = tf.nn.sigmoid_cross_entropy_with_logits(labels = tf.to_float(self.labels),
+                                                       logits = tf.squeeze(logits))
+
+        # put more weight on samples with label 1
+#        alpha = 0.266
+#        weights = tf.exp((tf.to_float(self.labels)-0.5)*2*alpha, name="weights")
+#        weighted_loss = tf.multiply(weights, loss, name="weighted_loss")
+#        self.loss = tf.reduce_sum(weighted_loss)/tf.reduce_sum(weights)
+        
+        # normal loss
+        self.loss = tf.reduce_mean(loss)
+
+        result_options = tf.concat([1-scores, scores], axis = 1)
+        self.predicts = tf.argmax(result_options, axis = 1)
+        self.probabilities = tf.reduce_max(result_options, axis = 1)
+
+
+    def add_train_op(self):
+        self.learning_rate = tf.Variable(self.config.learning_rate, trainable=False)
+        optimizer = self.config.optimizer(self.learning_rate)
+        grads_and_vars = optimizer.compute_gradients(self.loss)
+        with tf.variable_scope("clip_grad"):
+            capped_gvs = [(tf.clip_by_value(grad, -self.config.threshold, self.config.threshold), var)
+                            for grad, var in grads_and_vars]
+        self.train_op = optimizer.apply_gradients(capped_gvs, name="train")
+
+
+
+class BiPMModel(BiRNNModel):
+    pass
+
+
+
